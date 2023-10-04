232ad337392ee044b5d1409c7c38474305486a6b
AlejandroxSamuel <alejandro.aponte351@gmail.com>
2023-10-04 20:36:19 -0300

    Hello_world

--- hello_world_python.py
+++ hello_world_python.py
@@ -1,13 +1,13 @@
 # 1. TAREA: imprime "Hola, mundo"
 print("Hola,mundo")
 # 2. imprime "Hola, Noelle" con el nombre en una variable
-name = "Noelle"
+name = "Alejandro"
 print("Hello", name  )	# con una coma
 print( "Hello" + name )	# con un +
 # 3. imprimir "Hola 42!" con el número en una variable
-name = 42
-print( "Hola", name )	# con una coma
-print( "Hola" + -- name)	# con una +	-- este debería arrojar un error!
+number = 6
+print( "Hola", number )	# con una coma
+print( "Hola" + str(number))	# con una +	-- este debería arrojar un error!
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
